ca7b31fb9a576657c7354ed65acba43ae436bb2e
ajwestgerdes <ajwestgerdes@gmail.com>
2022-07-10 01:46:37 -0500

    style changes, updated card component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div style={{flexFlow: 'row wrap'}}>
+    <div style={{display: 'flex', width: '100%', height: '100%'}} className="App">
+      <div style={{ width: '50%', display: 'flex', flexWrap: 'wrap' }}>
         <AccessoryCard/>
         <AccessoryCard/>
         <AccessoryCard/>
--- src/components/AccessoryCard.js
+++ src/components/AccessoryCard.js
@@ -1,43 +1,48 @@
-import { Card, CardMedia, CardContent, Typography, TextField, CardActions, Button } from "@mui/material"
+import { useState } from "react"
+import { Card, CardContent,TextField } from "@mui/material"
+
+const min = 0
+const max = 6
 
 export const AccessoryCard = (props) => {
     return (
-        <Card sx={{maxWidth: '300px', flexFlow: 'column wrap'}}>
+        <Card sx={{ maxWidth: '300px', margin: '10px', backgroundColor: '#2c2f33' }}>
             <CardContent>
-                <div style={{flexFlow: 'row wrap', marginBottom: '10px'}}>
-                    <TextField sx={{maxWidth: '200px'}}  id="outlined-basic" label="Engraving 1" variant="outlined" />
-                    <TextField
-                        sx={{maxWidth: '60px', float: 'right'}}
-                        id="outlined-number"
-                        type="number"                      
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </div>
-                <div style={{flexFlow: 'row wrap', marginBottom: '10px'}}>
-                    <TextField sx={{maxWidth: '200px'}}  id="outlined-basic" label="Engraving 1" variant="outlined" />
-                    <TextField
-                        sx={{maxWidth: '60px', float: 'right'}}
-                        id="outlined-number"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </div>
-                <div style={{flexFlow: 'row wrap', marginBottom: '10px'}}>
-                    <TextField sx={{maxWidth: '200px'}}  id="outlined-basic" label="Engraving 1" variant="outlined" />
-                    <TextField
-                        sx={{maxWidth: '60px', float: 'right'}}
-                        id="outlined-number"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </div>
+                <EngravingField label="Engraving 1"/>
+                <EngravingField label="Engraving 2"/>
+                <EngravingField label="Negative Engraving"/>
             </CardContent>
         </Card>
     )   
+}
+
+const EngravingField = (props) => {
+    const [value, setValue] = useState(0);
+
+    return (
+        <div style={{ flexFlow: 'row wrap', marginBottom: '10px' }}>
+            <TextField sx={{ maxWidth: '200px' }}  id="outlined-basic" label={props.label} variant="outlined" />
+            <TextField
+                sx={{maxWidth: '60px', float: 'right'}}
+                id="outlined-number"
+                type="number"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                InputProps={{
+                    inputProps: { 
+                        min: min, max: max 
+                    }
+                }}
+                value={value}
+                onChange={(e) => {
+                    // TODO: this needs to change, you can still just spam 0 in the input
+                    var value = parseInt(e.target.value, 10);
+                    if (value > max) value = max;
+                    if (value < min) value = min;
+                    setValue(value);
+                }}
+            />
+        </div>
+    )
 }
