f3a2621ddea84d6f497383b3418dd39368be2730
ajwestgerdes <ajwestgerdes@gmail.com>
2022-07-11 21:07:21 -0500

    some styling changes, added more engravings to constants

--- src/components/AccessoryCard.js
+++ src/components/AccessoryCard.js
@@ -1,18 +1,18 @@
 import { useState } from "react"
 import { Card, CardContent, TextField, Autocomplete } from "@mui/material"
-import { negativeEngravings } from "../constants"
+import { negativeEngravings, mainEngravings, classEngravings } from "../constants"
 
 const min = 0
 const max = 6
 
-console.log(negativeEngravings)
+console.log(classEngravings)
 
 export const AccessoryCard = (props) => {
     return (
         <Card sx={{ minWidth: '300px', margin: '10px', backgroundColor: '#2c2f33' }}>
             <CardContent>
-                <EngravingField label="Engraving 1" engravings={negativeEngravings}/>
-                <EngravingField label="Engraving 2" engravings={negativeEngravings}/>
+                <EngravingField label="Engraving 1" engravings={mainEngravings}/>
+                <EngravingField label="Engraving 2" engravings={mainEngravings}/>
                 <EngravingField label="Negative Engraving" engravings={negativeEngravings}/>
             </CardContent>
         </Card>
@@ -21,23 +21,18 @@ export const AccessoryCard = (props) => {
 
 const EngravingField = (props) => {
     const [value, setValue] = useState(0);
-    const [dropdown, setDropdown] = useState('');
-
-  const handleChange = (event) => {
-    setDropdown(event.target.value);
-  };
 
     return (
         <div style={{flexFlow: 'row nowrap', marginBottom: '10px' }}>
             <Autocomplete
-                sx={{minWidth: '200px'}}
+                sx={{minWidth: '200px', float: 'left'}}
                 disablePortal
                 id="engraving-dropdown"
                 options={props.engravings}
-                renderInput={(params) => <TextField {...params} label={props.label} />}
+                renderInput={(params) => <TextField {...params} sx={{ marginBottom: '10px' }} label={props.label} />}
             />
             <TextField
-                sx={{maxWidth: '60px', float: 'right'}}
+                sx={{maxWidth: '60px', float: 'right', marginBottom: '10px'}}
                 id="outlined-number"
                 type="number"
                 InputLabelProps={{
@@ -45,7 +40,8 @@ const EngravingField = (props) => {
                 }}
                 InputProps={{
                     inputProps: { 
-                        min: min, max: max 
+                        min: min, 
+                        max: max 
                     }
                 }}
                 value={value}
--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,52 @@ export const negativeEngravings = [
     'Defense Decrease',
     'Speed Decrease'
 ]
+
+export const mainEngravings = [
+    'Adrenaline',
+    'Awakening',
+    'Barricade',
+    'Broken Bone',
+    'Masters Tenacity',
+    'Contender',
+    'Crisis Evasion',
+    'Crushing Fist',
+    'Cursed Doll',
+    'Divine Protection',
+    'Disrespect',
+    'Drops of Ether',
+    'Emergency Rescue',
+    'Enhanced Shield',
+    'Ether Enhancement',
+    'Explosive Expert',
+    'All-out Attack',
+    'Fortitude',
+    'Grudge',
+    'Heavy Armor Equipment',
+    'Max MP',
+    'Keen Blunt Weapon',
+    'Necromancy',
+    'Magick Stream',
+    'Increase Mass',
+    'Master Brawler',
+    'Ambush Master',
+    'Master of Escape',
+    'Hit Master',
+    'MP Regen',
+    'Shield Piercing',
+    'Precision Dagger',
+    'Preemptive Strike',
+    'Propulsion',
+    'Raid Captain',
+    'Expert',
+    'Spirit Absorption',
+    'Stabilized Status',
+    'Strong Will',
+    'Super Charge',
+    'Vital Point'
+]
+
+export const classEngravings = {
+    Berserker: ['Mayhem', 'Berserker Technique'],
+}    
+
