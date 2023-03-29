71f00aed5515fbdf576513fa220ff9f8d705ca32
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-03-28 23:31:22 -0500

    testing roster layout

--- src/pages/Roster/Roster.js
+++ src/pages/Roster/Roster.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import { Card, CardMedia, CardContent, Typography, CardActionArea } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, CardActionArea, Grid } from "@mui/material";
 import loaStriker from "../../assets/images/loaStriker.jpg"
 import { useState } from "react";
 
@@ -7,6 +7,7 @@ function Roster() {
     const [isBig, setIsBig] = useState(false)
 
     return (
+        <Grid>
         <motion.div 
             layout
             animate={{
@@ -18,7 +19,7 @@ function Roster() {
                 <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
                     <CardMedia
                         component="img"
-                        height="140"
+                        height="100%"
                         image={loaStriker}
                         alt="striker"
                     />
@@ -30,6 +31,99 @@ function Roster() {
                 </CardActionArea>
             </Card>
         </motion.div>
+        <motion.div 
+            layout
+            animate={{
+                height: isBig ? 1200 : 600,
+                width: isBig ? 1200 : 600,
+              }}
+        >
+            <Card>
+                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
+                    <CardMedia
+                        component="img"
+                        height="100%"
+                        image={loaStriker}
+                        alt="striker"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Striker
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </motion.div>
+        <motion.div 
+            layout
+            animate={{
+                height: isBig ? 1200 : 600,
+                width: isBig ? 1200 : 600,
+              }}
+        >
+            <Card>
+                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
+                    <CardMedia
+                        component="img"
+                        height="100%"
+                        image={loaStriker}
+                        alt="striker"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Striker
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </motion.div>
+        <motion.div 
+            layout
+            animate={{
+                height: isBig ? 1200 : 600,
+                width: isBig ? 1200 : 600,
+              }}
+        >
+            <Card>
+                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
+                    <CardMedia
+                        component="img"
+                        height="100%"
+                        image={loaStriker}
+                        alt="striker"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Striker
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </motion.div>
+        <motion.div 
+            layout
+            animate={{
+                height: isBig ? 1200 : 600,
+                width: isBig ? 1200 : 600,
+              }}
+        >
+            <Card>
+                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
+                    <CardMedia
+                        component="img"
+                        height="100%"
+                        image={loaStriker}
+                        alt="striker"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Striker
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </motion.div>
+        </Grid>
         
     )
 }
