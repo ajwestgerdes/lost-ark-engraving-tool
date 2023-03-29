be9ea834f2d3216ad230c1879dd69d53c026e3df
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-03-29 00:34:21 -0500

    changing roster cards to use header instead of media

--- src/pages/Roster/Roster.js
+++ src/pages/Roster/Roster.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import { Card, CardMedia, CardContent, Typography, CardActionArea, Grid } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, CardActionArea, CardHeader, Avatar, Grid } from "@mui/material";
 import loaStriker from "../../assets/images/loaStriker.jpg"
 import { useState } from "react";
 
@@ -16,110 +16,16 @@ function Roster() {
               }}
         >
             <Card>
-                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
-                    <CardMedia
-                        component="img"
-                        height="100%"
-                        image={loaStriker}
-                        alt="striker"
+                <CardActionArea 
+                    onClick={() => setIsBig((isBig) => !isBig)}
+                >
+                    <CardHeader
+                        avatar={
+                        <Avatar src={loaStriker}/>
+                        }
+                        title="Bigpumper7"
+                        subheader="1520"
                     />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Striker
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </motion.div>
-        <motion.div 
-            layout
-            animate={{
-                height: isBig ? 1200 : 600,
-                width: isBig ? 1200 : 600,
-              }}
-        >
-            <Card>
-                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
-                    <CardMedia
-                        component="img"
-                        height="100%"
-                        image={loaStriker}
-                        alt="striker"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Striker
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </motion.div>
-        <motion.div 
-            layout
-            animate={{
-                height: isBig ? 1200 : 600,
-                width: isBig ? 1200 : 600,
-              }}
-        >
-            <Card>
-                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
-                    <CardMedia
-                        component="img"
-                        height="100%"
-                        image={loaStriker}
-                        alt="striker"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Striker
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </motion.div>
-        <motion.div 
-            layout
-            animate={{
-                height: isBig ? 1200 : 600,
-                width: isBig ? 1200 : 600,
-              }}
-        >
-            <Card>
-                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
-                    <CardMedia
-                        component="img"
-                        height="100%"
-                        image={loaStriker}
-                        alt="striker"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Striker
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </motion.div>
-        <motion.div 
-            layout
-            animate={{
-                height: isBig ? 1200 : 600,
-                width: isBig ? 1200 : 600,
-              }}
-        >
-            <Card>
-                <CardActionArea onClick={() => setIsBig((isBig) => !isBig)}>
-                    <CardMedia
-                        component="img"
-                        height="100%"
-                        image={loaStriker}
-                        alt="striker"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Striker
-                        </Typography>
-                    </CardContent>
                 </CardActionArea>
             </Card>
         </motion.div>
