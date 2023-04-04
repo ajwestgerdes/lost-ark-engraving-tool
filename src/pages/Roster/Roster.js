44d8af840162cf48d6229b58f54ec3bc12a4e79c
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-04-03 21:43:00 -0500

    still messing with what is best for roster cards

--- src/pages/Roster/Roster.js
+++ src/pages/Roster/Roster.js
@@ -1,36 +1,37 @@
 import { motion } from "framer-motion"
-import { Card, CardMedia, CardContent, Typography, CardActionArea, CardHeader, Avatar, Grid } from "@mui/material";
+import { Card, CardMedia, Box, CardContent, Typography, CardActionArea, CardHeader, Avatar, Grid } from "@mui/material";
 import loaStriker from "../../assets/images/loaStriker.jpg"
 import { useState } from "react";
 
-function Roster() {
-    const [isBig, setIsBig] = useState(false)
-
+export default function Roster() {
+    const [isBig, setIsBig] = useState(false);
+  
+    const handleCardClick = () => {
+      setIsBig(true);
+    };
+  
+    const handleExpandedCardClick = () => {
+      setIsBig(false);
+    };
+  
     return (
-        <Grid>
-        <motion.div 
-            layout
-            animate={{
-                height: isBig ? 1200 : 600,
-                width: isBig ? 1200 : 600,
-              }}
-        >
-            <Card>
-                <CardActionArea 
-                    onClick={() => setIsBig((isBig) => !isBig)}
-                >
-                    <CardHeader
-                        avatar={
-                        <Avatar src={loaStriker}/>
-                        }
-                        title="Bigpumper7"
-                        subheader="1520"
-                    />
-                </CardActionArea>
-            </Card>
-        </motion.div>
-        </Grid>
-        
-    )
-}
-export default Roster;
+      <Grid container spacing={2}>
+        <CardContent>
+          <Box display="flex" alignItems="center">
+            <Avatar src={loaStriker} />
+            <Typography variant="h5" component="h2" sx={{ ml: 2 }}>
+              Bigpumper7
+            </Typography>
+          </Box>
+          <Box display="flex" alignItems="center" sx={{ mt: 2 }}>
+            <Typography variant="h6" component="h3" sx={{ mr: 2 }}>
+              Rating:
+            </Typography>
+            <Typography variant="h6" component="h3">
+              1520
+            </Typography>
+          </Box>
+        </CardContent>
+      </Grid>
+    );
+  }
